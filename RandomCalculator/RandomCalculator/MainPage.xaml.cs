d97349c4746bb437c7163d7d8de67473d4cdcc5f
lscantillo <lscantillo@uninorte.edu.co>
2017-10-09 17:26:08 -0500

    first commit

---
+++ RandomCalculator/RandomCalculator/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RandomCalculator
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            Title = "Main Page";
+            InitializeComponent();
+           
+        }
+
+        
+        void Clickstart(object sender, System.EventArgs e)
+        {
+            var number = new Label();
+            number.SetBinding(Label.TextProperty, new Binding("SelectedItem", source: picker));
+            Navigation.PushAsync(new SecondPage(number.Text.ToString()));
+
+        }
+
+       
+
+    }
+}
---
+++ RandomCalculator/RandomCalculator/SecondPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RandomCalculator
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage (String value)
+		{
+            Title ="Operaciones";
+			InitializeComponent ();
+            int counter = 1;
+            titulo.Text = "Operación " + counter + "de " + value + ".";
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+            Random rand3 = new Random();
+            number1.Text = rand1.Next(0, 100).ToString();
+            number2.Text = rand2.Next(0, 50).ToString();
+
+            if (rand3.Next(0, 2) == 1)
+            {
+                operacion.Text = "+";
+            }
+            else
+            {
+                operacion.Text = "-";
+            }
+
+        }
+
+        //void Clickrespuesta(object sender, System.EventArgs e)
+        void Clickrespuesta(String value, object sender, System.EventArgs e)
+        {
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+            Random rand3 = new Random();
+            //int x = Convert.ToInt32(value);
+            //while (int i = 0; i< x; i++) { }
+            number1.Text = rand1.Next(0, 100).ToString();
+            number2.Text = rand2.Next(0, 50).ToString();
+
+            if (rand3.Next(0, 2) == 1)
+            {
+                operacion.Text = "+";
+            }
+            else
+            {
+                operacion.Text = "-";
+            }
+            etresultado.Text = "";
+           
+
+
+
+        }
+    }
+}
